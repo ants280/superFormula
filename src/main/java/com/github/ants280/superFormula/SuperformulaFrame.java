dd84b5a3234696474dd997741854135456353a6c
Jacob Patterson <jacob.patterson@gmail.com>
2018-11-18 21:41:25 -0800

    changing the frame to use the UiManager for ui logic.

--- src/main/java/com/github/ants280/superFormula/SuperformulaFrame.java
+++ src/main/java/com/github/ants280/superFormula/SuperformulaFrame.java
@@ -1,9 +1,6 @@
 package com.github.ants280.superFormula;
 
-import static com.github.ants280.superFormula.SuperformulaModel.*;
 import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -13,112 +10,125 @@ import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
-import javax.swing.JSpinner;
 import javax.swing.KeyStroke;
-import javax.swing.SpinnerNumberModel;
 import javax.swing.SwingConstants;
 import javax.swing.Timer;
 import javax.swing.WindowConstants;
-import javax.swing.event.ChangeEvent;
 
 public class SuperformulaFrame extends JFrame
 {
-	private static final String MI_START = "Start changing";
-	private static final String MI_STOP = "Stop changing";
-	private static final String MI_CUSTOM_MODEL = "Add custom shape...";
-	private static final String MI_SHOW_DEMO = "Show demo shapes";
-	private static final String MI_SHOW_RANDOM = "Show random shapes";
-	private static final String MI_SIZE_UP = "Increase canvas size";
-	private static final String MI_SIZE_DOWN = "Decrease canvas size";
-	private static final String MI_HELP = "Help...";
-	private static final String MI_ABOUT = "About...";
 	private static final long serialVersionUID = 1L;
-
-	private final SuperformulaModel model;
-	private final SuperformulaView view;
-	private final SuperformulaController controller;
-
-	// statefull controls:
-	private final Timer mutatorTimer;
-	private final JLabel variablesLabel;
-	private JSlider speedSlider;
-	private boolean showWikipediaDemo;
-	private JMenuItem startStopMenuItem;
-	private JButton startStopButton;
-	private JMenuItem showWikipediaDemoMenuItem;
-	private JButton showWikipediaDemoButton;
 
 	public SuperformulaFrame()
 	{
 		super("Superformula");
 
-		model = new SuperformulaModel();
-		view = new SuperformulaView();
-		controller = new SuperformulaController(model, view);
-		controller.changeSize(5);
-		speedSlider = new JSlider(); // must be called before createButtonMenu.
-		variablesLabel = new JLabel();
-		variablesLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		updateVariablesLabel();
-		showWikipediaDemo = false;
+		JMenuItem startStopMenuItem = new JMenuItem();
+		JMenuItem showWikipediaDemoMenuItem = new JMenuItem();
+		JMenuItem customModelMenuItem = new JMenuItem();
+		JMenuItem sizeUpMenuItem = new JMenuItem();
+		JMenuItem sizeDownMenuItem = new JMenuItem();
+		JMenuItem helpMenuItem = new JMenuItem();
+		JMenuItem aboutMenuItem = new JMenuItem();
+		JMenuBar menuBar = createMainMenu(
+				startStopMenuItem,
+				showWikipediaDemoMenuItem,
+				customModelMenuItem,
+				sizeUpMenuItem,
+				sizeDownMenuItem,
+				helpMenuItem,
+				aboutMenuItem);
 
-		init();
-		mutatorTimer = createTimer(speedSlider.getValue());
+		JLabel variablesLabel = new JLabel();
+
+		variablesLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+		SuperformulaModel model = new SuperformulaModel();
+		SuperformulaView view = new SuperformulaView();
+		SuperformulaController controller
+				= new SuperformulaController(model, view);
+
+		JButton startStopButton = new JButton();
+		JButton showWikipediaDemoButton = new JButton();
+		JSlider speedSlider = new JSlider(SwingConstants.HORIZONTAL, 10, 3_000, 300);
+		speedSlider.setMajorTickSpacing(100);
+		speedSlider.setMinorTickSpacing(50);
+		speedSlider.setSnapToTicks(true);
+		speedSlider.setPaintTicks(true);
+
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.add(startStopButton);
+		buttonPanel.add(showWikipediaDemoButton);
+		buttonPanel.add(speedSlider);
+
+		Timer mutatorTimer = new Timer(speedSlider.getValue(), null);
+
+		SuperformulaUiManager.create(
+				model,
+				controller,
+				this,
+				startStopMenuItem,
+				showWikipediaDemoMenuItem,
+				customModelMenuItem,
+				sizeUpMenuItem,
+				sizeDownMenuItem,
+				helpMenuItem,
+				aboutMenuItem,
+				variablesLabel,
+				startStopButton,
+				showWikipediaDemoButton,
+				speedSlider,
+				mutatorTimer,
+				rootPaneCheckingEnabled);
+
+		init(
+				menuBar,
+				variablesLabel,
+				view,
+				buttonPanel,
+				mutatorTimer);
 
 		controller.update();
 	}
 
-	private void init()
+	private void init(
+			JMenuBar menuBar,
+			JLabel variablesLabel,
+			SuperformulaView superFormulaDisplayComponent,
+			JPanel buttonPanel,
+			Timer mutatorTimer)
 	{
-		this.setJMenuBar(createMainMenu());
+		this.addWindowListener(new SuperformulaWindowListener(mutatorTimer));
+
+		this.setJMenuBar(menuBar);
 		this.add(variablesLabel, BorderLayout.NORTH);
-		this.add(view);
-		this.add(createButtonMenu(), BorderLayout.SOUTH);
+		this.add(superFormulaDisplayComponent);
+		this.add(buttonPanel, BorderLayout.SOUTH);
 
 		this.pack();
 		this.setResizable(false);
 		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 	}
 
-	private Timer createTimer(int delay)
+	private JMenuBar createMainMenu(
+			JMenuItem startStopMenuItem,
+			JMenuItem showWikipediaDemoMenuItem,
+			JMenuItem customModelMenuItem,
+			JMenuItem sizeUpMenuItem,
+			JMenuItem sizeDownMenuItem,
+			JMenuItem helpMenuItem,
+			JMenuItem aboutMenuItem)
 	{
-		final Timer timer = new Timer(delay, this::mutate);
-		this.addWindowListener(new SuperFormulaWindowListener(timer));
-
-		return timer;
-	}
-
-	private void mutate(ActionEvent event)
-	{
-		controller.mutate();
-		this.update();
-	}
-
-	private void update()
-	{
-		this.updateVariablesLabel();
-		controller.update();
-	}
-
-	private JMenuBar createMainMenu()
-	{
-		startStopMenuItem
-				= createMenuItem(MI_START, KeyEvent.VK_S);
-		showWikipediaDemoMenuItem
-				= createMenuItem(MI_SHOW_DEMO, KeyEvent.VK_D);
-		JMenuItem customModelMenuItem
-				= createMenuItem(MI_CUSTOM_MODEL, KeyEvent.VK_C);
-		JMenuItem sizeUpMenuItem
-				= createMenuItem(MI_SIZE_UP, KeyEvent.VK_UP);
-		JMenuItem sizeDownMenuItem
-				= createMenuItem(MI_SIZE_DOWN, KeyEvent.VK_DOWN);
-		JMenuItem helpMenuItem
-				= createMenuItem(MI_HELP, KeyEvent.VK_F1);
-		JMenuItem aboutMenuItem
-				= createMenuItem(MI_ABOUT, KeyEvent.VK_A);
+		// TODO: should all menu objects use mnemonics or accelerators?
+		startStopMenuItem.setAccelerator(createKeyStroke(KeyEvent.VK_S));
+		showWikipediaDemoMenuItem.setAccelerator(createKeyStroke(KeyEvent.VK_D));
+		customModelMenuItem.setAccelerator(createKeyStroke(KeyEvent.VK_C));
+		sizeUpMenuItem.setAccelerator(createKeyStroke(KeyEvent.VK_UP));
+		sizeDownMenuItem.setAccelerator(createKeyStroke(KeyEvent.VK_DOWN));
+		helpMenuItem.setAccelerator(createKeyStroke(KeyEvent.VK_F1));
+		aboutMenuItem.setAccelerator(createKeyStroke(KeyEvent.VK_A));
 
 		JMenu fileMenu = new JMenu("File");
 		fileMenu.add(startStopMenuItem);
@@ -142,203 +152,16 @@ public class SuperformulaFrame extends JFrame
 		return mainMenu;
 	}
 
-	private JMenuItem createMenuItem(String name, int mnemonic)
+	private static KeyStroke createKeyStroke(int mnemonic)
 	{
-		JMenuItem menuItem = new JMenuItem(name, mnemonic);
-
-		menuItem.setAccelerator(KeyStroke.getKeyStroke(mnemonic,
-				KeyEvent.ALT_DOWN_MASK));
-		menuItem.addActionListener(this::handleMenuClick);
-
-		return menuItem;
+		return KeyStroke.getKeyStroke(mnemonic, KeyEvent.ALT_DOWN_MASK);
 	}
 
-	private JPanel createButtonMenu()
-	{
-		startStopButton = createButton(MI_START);
-		showWikipediaDemoButton = createButton(MI_SHOW_DEMO);
-		speedSlider = new JSlider(SwingConstants.HORIZONTAL, 10, 3_000, 300);
-		speedSlider.setMajorTickSpacing(100);
-		speedSlider.setMinorTickSpacing(50);
-		speedSlider.setSnapToTicks(true);
-		speedSlider.setPaintTicks(true);
-		speedSlider.addChangeListener(this::updateMutatorDelay);
-
-		JPanel buttonPanel = new JPanel();
-		buttonPanel.add(startStopButton);
-		buttonPanel.add(showWikipediaDemoButton);
-		buttonPanel.add(speedSlider);
-
-		return buttonPanel;
-	}
-
-	private JButton createButton(String text)
-	{
-		JButton button = new JButton(text);
-		button.addActionListener(this::handleMenuClick);
-		return button;
-	}
-
-	private void updateMutatorDelay(ChangeEvent changeEvent)
-	{
-		int value = speedSlider.getValue();
-		mutatorTimer.setDelay(value);
-		mutatorTimer.restart();
-	}
-
-	private void updateVariablesLabel()
-	{
-		// TODO: It would be nice if we could edit the values here.
-		variablesLabel.setText(String.format(
-				"a = %d    b = %d    m = %d    n1 = %d    n2 = %d    n3 = %d",
-				model.getA(), model.getB(),
-				model.getM(), model.getN1(), model.getN2(), model.getN3()));
-	}
-
-	public void handleMenuClick(ActionEvent event)
-	{
-		final String command = event.getActionCommand();
-		if (command == null)
-		{
-			return;
-		}
-
-		switch (command)
-		{
-			case MI_START:
-			case MI_STOP:
-				boolean isRunning = mutatorTimer.isRunning();
-				if (isRunning)
-				{
-					mutatorTimer.stop();
-					startStopMenuItem.setText(MI_START);
-					startStopButton.setText(MI_START);
-				}
-				else
-				{
-					mutatorTimer.start();
-					startStopMenuItem.setText(MI_STOP);
-					startStopButton.setText(MI_STOP);
-				}
-				break;
-			case MI_SHOW_DEMO:
-			case MI_SHOW_RANDOM:
-				showWikipediaDemo = !showWikipediaDemo;
-				if (showWikipediaDemoButton != null)
-				{
-					if (showWikipediaDemo)
-					{
-						showWikipediaDemoMenuItem.setText(MI_SHOW_RANDOM);
-						showWikipediaDemoButton.setText(MI_SHOW_RANDOM);
-					}
-					else
-					{
-						showWikipediaDemoMenuItem.setText(MI_SHOW_DEMO);
-						showWikipediaDemoButton.setText(MI_SHOW_DEMO);
-					}
-				}
-				controller.setShowWikipediaDemos(showWikipediaDemo);
-				update();
-				break;
-			case MI_CUSTOM_MODEL:
-				handleCustomModelCommand();
-				break;
-			case MI_SIZE_UP:
-				changeCanvasSize(1);
-				break;
-			case MI_SIZE_DOWN:
-				changeCanvasSize(-1);
-				break;
-			case MI_HELP:
-				// Blocking:
-				JOptionPane.showMessageDialog(SuperformulaFrame.this,
-						"<html>"
-						+ "<body>"
-						+ "Check out the "
-						+ "<a href=https://en.wikipedia.org/wiki/Superformula>"
-						+ "Wikipedia page"
-						+ "</a>"
-						+ " on Superformulas"
-						+ "</body>"
-						+ "</html>",
-						"Help for " + SuperformulaFrame.this.getTitle(),
-						JOptionPane.QUESTION_MESSAGE);
-				break;
-			case MI_ABOUT:
-				// Blocking:
-				JOptionPane.showMessageDialog(SuperformulaFrame.this,
-						"By Jacob Patterson",
-						"About " + SuperformulaFrame.this.getTitle(),
-						JOptionPane.INFORMATION_MESSAGE);
-				break;
-			default:
-				throw new IllegalArgumentException(String.format(
-						"Don't know how to handle an action of %s.", command));
-		}
-	}
-
-	private void changeCanvasSize(int delta)
-	{
-		boolean sizeChanged = controller.changeSize(delta);
-		if (sizeChanged)
-		{
-			this.pack();
-			controller.update();
-		}
-	}
-
-	private void handleCustomModelCommand()
-	{
-		boolean wasRunning = mutatorTimer.isRunning();
-		if (wasRunning)
-		{
-			mutatorTimer.stop();
-		}
-
-		final SpinnerNumberModel mSpinnerModel
-				= new SpinnerNumberModel(model.getM(), M_MIN, M_MAX, 1);
-		final SpinnerNumberModel n1SpinnerModel
-				= new SpinnerNumberModel(model.getN1(), N1_MIN, N1_MAX, 1);
-		final SpinnerNumberModel n2SpinnerModel
-				= new SpinnerNumberModel(model.getN2(), N2_MIN, N2_MAX, 1);
-		final SpinnerNumberModel n3SpinnerModel
-				= new SpinnerNumberModel(model.getN3(), N3_MIN, N3_MAX, 1);
-		JPanel customModelPanel = new JPanel(new GridLayout(4, 2));
-		customModelPanel.add(new JLabel("m"));
-		customModelPanel.add(new JSpinner(mSpinnerModel));
-		customModelPanel.add(new JLabel("n1"));
-		customModelPanel.add(new JSpinner(n1SpinnerModel));
-		customModelPanel.add(new JLabel("n2"));
-		customModelPanel.add(new JSpinner(n2SpinnerModel));
-		customModelPanel.add(new JLabel("n3"));
-		customModelPanel.add(new JSpinner(n3SpinnerModel));
-		// Blocking:
-		int choice = JOptionPane.showConfirmDialog(SuperformulaFrame.this,
-				customModelPanel,
-				"Custom shape for " + SuperformulaFrame.this.getTitle(),
-				JOptionPane.OK_CANCEL_OPTION);
-		if (choice == JOptionPane.OK_OPTION)
-		{
-			startStopMenuItem.setText(MI_START);
-			startStopButton.setText(MI_START);
-
-			model.setM(mSpinnerModel.getNumber().intValue());
-			model.setN1(n1SpinnerModel.getNumber().intValue());
-			model.setN2(n2SpinnerModel.getNumber().intValue());
-			model.setN3(n3SpinnerModel.getNumber().intValue());
-			this.update();
-		}
-		else if (wasRunning)
-		{
-			mutatorTimer.start();
-		}
-	}
-
-	private class SuperFormulaWindowListener extends WindowAdapter
+	private class SuperformulaWindowListener extends WindowAdapter
 	{
 		private final Timer timer;
 
-		public SuperFormulaWindowListener(Timer timer)
+		public SuperformulaWindowListener(Timer timer)
 		{
 			this.timer = timer;
 		}
--- src/main/java/com/github/ants280/superFormula/SuperformulaUiManager.java
+++ src/main/java/com/github/ants280/superFormula/SuperformulaUiManager.java
@@ -1,30 +1,23 @@
 package com.github.ants280.superFormula;
 
 import static com.github.ants280.superFormula.SuperformulaModel.*;
-import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.JSpinner;
-import javax.swing.KeyStroke;
 import javax.swing.SpinnerNumberModel;
-import javax.swing.SwingConstants;
 import javax.swing.Timer;
-import javax.swing.WindowConstants;
 import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
-public class SuperformulaFrame extends JFrame
+public class SuperformulaUiManager implements ActionListener, ChangeListener
 {
 	private static final String MI_START = "Start changing";
 	private static final String MI_STOP = "Stop changing";
@@ -35,63 +28,97 @@ public class SuperformulaFrame extends JFrame
 	private static final String MI_SIZE_DOWN = "Decrease canvas size";
 	private static final String MI_HELP = "Help...";
 	private static final String MI_ABOUT = "About...";
-	private static final long serialVersionUID = 1L;
 
 	private final SuperformulaModel model;
-	private final SuperformulaView view;
 	private final SuperformulaController controller;
 
-	// statefull controls:
-	private final Timer mutatorTimer;
+	private final JFrame parentComponent;
+	private final JMenuItem startStopMenuItem;
+	private final JMenuItem showWikipediaDemoMenuItem;
+	private final JMenuItem customModelMenuItem;
+	private final JMenuItem sizeUpMenuItem;
+	private final JMenuItem sizeDownMenuItem;
+	private final JMenuItem helpMenuItem;
+	private final JMenuItem aboutMenuItem;
 	private final JLabel variablesLabel;
-	private JSlider speedSlider;
+	private final JButton startStopButton;
+	private final JButton showWikipediaDemoButton;
+	private final JSlider speedSlider;
+	private final Timer mutatorTimer;
 	private boolean showWikipediaDemo;
-	private JMenuItem startStopMenuItem;
-	private JButton startStopButton;
-	private JMenuItem showWikipediaDemoMenuItem;
-	private JButton showWikipediaDemoButton;
 
-	public SuperformulaFrame()
+	private SuperformulaUiManager(
+			SuperformulaModel model,
+			SuperformulaController controller,
+			JFrame parentComponent,
+			JMenuItem startStopMenuItem,
+			JMenuItem showWikipediaDemoMenuItem,
+			JMenuItem customModelMenuItem,
+			JMenuItem sizeUpMenuItem,
+			JMenuItem sizeDownMenuItem,
+			JMenuItem helpMenuItem,
+			JMenuItem aboutMenuItem,
+			JLabel variablesLabel,
+			JButton startStopButton,
+			JButton showWikipediaDemoButton,
+			JSlider speedSlider,
+			Timer mutatorTimer,
+			boolean showWikipediaDemo)
 	{
-		super("Superformula");
+		this.model = model;
+		this.controller = controller;
 
-		model = new SuperformulaModel();
-		view = new SuperformulaView();
-		controller = new SuperformulaController(model, view);
-		controller.changeSize(5);
-		speedSlider = new JSlider(); // must be called before createButtonMenu.
-		variablesLabel = new JLabel();
-		variablesLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		updateVariablesLabel();
-		showWikipediaDemo = false;
+		this.parentComponent = parentComponent;
+		this.startStopMenuItem = startStopMenuItem;
+		this.showWikipediaDemoMenuItem = showWikipediaDemoMenuItem;
+		this.customModelMenuItem = customModelMenuItem;
+		this.sizeUpMenuItem = sizeUpMenuItem;
+		this.sizeDownMenuItem = sizeDownMenuItem;
+		this.helpMenuItem = helpMenuItem;
+		this.aboutMenuItem = aboutMenuItem;
+		this.variablesLabel = variablesLabel;
+		this.startStopButton = startStopButton;
+		this.showWikipediaDemoButton = showWikipediaDemoButton;
+		this.speedSlider = speedSlider;
 
-		init();
-		mutatorTimer = createTimer(speedSlider.getValue());
-
-		controller.update();
+		this.mutatorTimer = mutatorTimer;
+		this.showWikipediaDemo = false;
 	}
 
-	private void init()
+	private void initialize()
 	{
-		this.setJMenuBar(createMainMenu());
-		this.add(variablesLabel, BorderLayout.NORTH);
-		this.add(view);
-		this.add(createButtonMenu(), BorderLayout.SOUTH);
+		// MenuItems:
+		startStopMenuItem.setText(MI_START);
+		showWikipediaDemoMenuItem.setText(MI_SHOW_DEMO);
+		customModelMenuItem.setText(MI_CUSTOM_MODEL);
+		sizeUpMenuItem.setText(MI_SIZE_UP);
+		sizeDownMenuItem.setText(MI_SIZE_DOWN);
+		helpMenuItem.setText(MI_HELP);
+		aboutMenuItem.setText(MI_ABOUT);
 
-		this.pack();
-		this.setResizable(false);
-		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		startStopMenuItem.addActionListener(this);
+		showWikipediaDemoMenuItem.addActionListener(this);
+		customModelMenuItem.addActionListener(this);
+		sizeUpMenuItem.addActionListener(this);
+		sizeDownMenuItem.addActionListener(this);
+		helpMenuItem.addActionListener(this);
+		aboutMenuItem.addActionListener(this);
+
+		// Buttons:
+		startStopButton.setText(MI_START);
+		showWikipediaDemoButton.setText(MI_SHOW_DEMO);
+
+		startStopButton.addActionListener(this);
+		showWikipediaDemoButton.addActionListener(this);
+
+		// Slider:
+		speedSlider.addChangeListener(this);
+
+		// Timer:
+		mutatorTimer.addActionListener(actionEvent -> this.mutate());
 	}
 
-	private Timer createTimer(int delay)
-	{
-		final Timer timer = new Timer(delay, this::mutate);
-		this.addWindowListener(new SuperFormulaWindowListener(timer));
-
-		return timer;
-	}
-
-	private void mutate(ActionEvent event)
+	private void mutate()
 	{
 		controller.mutate();
 		this.update();
@@ -103,83 +130,8 @@ public class SuperformulaFrame extends JFrame
 		controller.update();
 	}
 
-	private JMenuBar createMainMenu()
-	{
-		startStopMenuItem
-				= createMenuItem(MI_START, KeyEvent.VK_S);
-		showWikipediaDemoMenuItem
-				= createMenuItem(MI_SHOW_DEMO, KeyEvent.VK_D);
-		JMenuItem customModelMenuItem
-				= createMenuItem(MI_CUSTOM_MODEL, KeyEvent.VK_C);
-		JMenuItem sizeUpMenuItem
-				= createMenuItem(MI_SIZE_UP, KeyEvent.VK_UP);
-		JMenuItem sizeDownMenuItem
-				= createMenuItem(MI_SIZE_DOWN, KeyEvent.VK_DOWN);
-		JMenuItem helpMenuItem
-				= createMenuItem(MI_HELP, KeyEvent.VK_F1);
-		JMenuItem aboutMenuItem
-				= createMenuItem(MI_ABOUT, KeyEvent.VK_A);
-
-		JMenu fileMenu = new JMenu("File");
-		fileMenu.add(startStopMenuItem);
-		fileMenu.add(showWikipediaDemoMenuItem);
-		fileMenu.add(customModelMenuItem);
-		fileMenu.setMnemonic(KeyEvent.VK_F);
-		JMenu settingsMenu = new JMenu("Settings");
-		settingsMenu.add(sizeUpMenuItem);
-		settingsMenu.add(sizeDownMenuItem);
-		settingsMenu.setMnemonic(KeyEvent.VK_S);
-		JMenu helpMenu = new JMenu("Help");
-		helpMenu.add(helpMenuItem);
-		helpMenu.add(aboutMenuItem);
-		helpMenu.setMnemonic(KeyEvent.VK_H);
-
-		JMenuBar mainMenu = new JMenuBar();
-		mainMenu.add(fileMenu);
-		mainMenu.add(settingsMenu);
-		mainMenu.add(helpMenu);
-
-		return mainMenu;
-	}
-
-	private JMenuItem createMenuItem(String name, int mnemonic)
-	{
-		JMenuItem menuItem = new JMenuItem(name, mnemonic);
-
-		menuItem.setAccelerator(KeyStroke.getKeyStroke(mnemonic,
-				KeyEvent.ALT_DOWN_MASK));
-		menuItem.addActionListener(this::handleMenuClick);
-
-		return menuItem;
-	}
-
-	private JPanel createButtonMenu()
-	{
-		startStopButton = createButton(MI_START);
-		showWikipediaDemoButton = createButton(MI_SHOW_DEMO);
-		speedSlider = new JSlider(SwingConstants.HORIZONTAL, 10, 3_000, 300);
-		speedSlider.setMajorTickSpacing(100);
-		speedSlider.setMinorTickSpacing(50);
-		speedSlider.setSnapToTicks(true);
-		speedSlider.setPaintTicks(true);
-		speedSlider.addChangeListener(this::updateMutatorDelay);
-
-		JPanel buttonPanel = new JPanel();
-		buttonPanel.add(startStopButton);
-		buttonPanel.add(showWikipediaDemoButton);
-		buttonPanel.add(speedSlider);
-
-		return buttonPanel;
-	}
-
-	private JButton createButton(String text)
-	{
-		JButton button = new JButton(text);
-		button.addActionListener(this::handleMenuClick);
-		return button;
-	}
-
-	private void updateMutatorDelay(ChangeEvent changeEvent)
+	@Override
+	public void stateChanged(ChangeEvent changeEvent)
 	{
 		int value = speedSlider.getValue();
 		mutatorTimer.setDelay(value);
@@ -195,9 +147,10 @@ public class SuperformulaFrame extends JFrame
 				model.getM(), model.getN1(), model.getN2(), model.getN3()));
 	}
 
-	public void handleMenuClick(ActionEvent event)
+	@Override
+	public void actionPerformed(ActionEvent actionEvent)
 	{
-		final String command = event.getActionCommand();
+		final String command = actionEvent.getActionCommand();
 		if (command == null)
 		{
 			return;
@@ -251,7 +204,7 @@ public class SuperformulaFrame extends JFrame
 				break;
 			case MI_HELP:
 				// Blocking:
-				JOptionPane.showMessageDialog(SuperformulaFrame.this,
+				JOptionPane.showMessageDialog(parentComponent,
 						"<html>"
 						+ "<body>"
 						+ "Check out the "
@@ -261,14 +214,14 @@ public class SuperformulaFrame extends JFrame
 						+ " on Superformulas"
 						+ "</body>"
 						+ "</html>",
-						"Help for " + SuperformulaFrame.this.getTitle(),
+						"Help for " + parentComponent.getTitle(),
 						JOptionPane.QUESTION_MESSAGE);
 				break;
 			case MI_ABOUT:
 				// Blocking:
-				JOptionPane.showMessageDialog(SuperformulaFrame.this,
+				JOptionPane.showMessageDialog(parentComponent,
 						"By Jacob Patterson",
-						"About " + SuperformulaFrame.this.getTitle(),
+						"About " + parentComponent.getTitle(),
 						JOptionPane.INFORMATION_MESSAGE);
 				break;
 			default:
@@ -282,7 +235,7 @@ public class SuperformulaFrame extends JFrame
 		boolean sizeChanged = controller.changeSize(delta);
 		if (sizeChanged)
 		{
-			this.pack();
+			parentComponent.pack();
 			controller.update();
 		}
 	}
@@ -313,9 +266,9 @@ public class SuperformulaFrame extends JFrame
 		customModelPanel.add(new JLabel("n3"));
 		customModelPanel.add(new JSpinner(n3SpinnerModel));
 		// Blocking:
-		int choice = JOptionPane.showConfirmDialog(SuperformulaFrame.this,
+		int choice = JOptionPane.showConfirmDialog(parentComponent,
 				customModelPanel,
-				"Custom shape for " + SuperformulaFrame.this.getTitle(),
+				"Custom shape for " + parentComponent.getTitle(),
 				JOptionPane.OK_CANCEL_OPTION);
 		if (choice == JOptionPane.OK_OPTION)
 		{
@@ -334,19 +287,41 @@ public class SuperformulaFrame extends JFrame
 		}
 	}
 
-	private class SuperFormulaWindowListener extends WindowAdapter
+	public static void create(SuperformulaModel model,
+			SuperformulaController controller,
+			SuperformulaFrame aThis,
+			JMenuItem startStopMenuItem,
+			JMenuItem showWikipediaDemoMenuItem,
+			JMenuItem customModelMenuItem,
+			JMenuItem sizeUpMenuItem,
+			JMenuItem sizeDownMenuItem,
+			JMenuItem helpMenuItem,
+			JMenuItem aboutMenuItem,
+			JLabel variablesLabel,
+			JButton startStopButton,
+			JButton showWikipediaDemoButton,
+			JSlider speedSlider,
+			Timer mutatorTimer,
+			boolean rootPaneCheckingEnabled)
 	{
-		private final Timer timer;
+		SuperformulaUiManager superformulaUiManager = new SuperformulaUiManager(
+				model,
+				controller,
+				aThis,
+				startStopMenuItem,
+				showWikipediaDemoMenuItem,
+				customModelMenuItem,
+				sizeUpMenuItem,
+				sizeDownMenuItem,
+				helpMenuItem,
+				aboutMenuItem,
+				variablesLabel,
+				startStopButton,
+				showWikipediaDemoButton,
+				speedSlider,
+				mutatorTimer,
+				rootPaneCheckingEnabled);
 
-		public SuperFormulaWindowListener(Timer timer)
-		{
-			this.timer = timer;
-		}
-
-		@Override
-		public void windowClosing(WindowEvent windowEvent)
-		{
-			timer.stop();
-		}
+		superformulaUiManager.initialize();
 	}
 }
